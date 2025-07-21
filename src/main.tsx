565d6fa7974f0650f5fbdd302aa9e881b4ee6e47
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2025-07-21 20:43:15 +0400

    fix/tree : fix config of files

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
-import App from "./App.tsx";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("Failed to find the root element");
+}
+
+const root = createRoot(container);
+root.render(<App />);
